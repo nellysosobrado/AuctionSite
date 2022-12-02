1ec2da981fa15a57fb338a3e667ce9a7bd3b7d03
Richard Chalk <barmybrit@gmail.com>
2022-12-02 10:23:17 +0100

    Facit Done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,24 +15,24 @@ public class Program
         // "Server=localhost;Database=RichardsAuctionSite;Trusted_Connection=True;TrustServerCertificate=true" Microsoft.EntityFrameworkCore.SqlServer - OutputDir Data
 
         // 3: Nu har vi möjlighet att loopa igenom datat i vår databas
-        using (var dbContext = new RichardsAuctionSiteContext())
-        {
-            foreach (var user in dbContext.Users.OrderBy(u => u.City))
-            {
-                Console.WriteLine($"{user.Name}: {user.City}");
-            }
-        }
+        //using (var dbContext = new RichardsAuctionSiteContext())
+        //{
+        //    foreach (var user in dbContext.Users.OrderBy(u => u.City))
+        //    {
+        //        Console.WriteLine($"{user.Name}: {user.City}");
+        //    }
+        //}
 
         //// 6: Create a builder to access appsettings.json
-        //var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
-        //var config = builder.Build();
+        var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
+        var config = builder.Build();
 
         //// Hämta connection string från appsettings.json
-        //var connectionString = config.GetConnectionString("DefaultConnection");
+        var connectionString = config.GetConnectionString("DefaultConnection");
 
         //// Starta vår app med connectionstring som en parameter
-        //var application = new Application(connectionString);
-        //application.Run();
+        var application = new Application(connectionString);
+        application.Run();
 
     }
 }
