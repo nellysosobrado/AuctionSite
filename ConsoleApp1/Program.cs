6f1355d32cbe9f8eb96970f3c5f02976ff30f479
Richard Chalk <barmybrit@gmail.com>
2022-12-02 10:27:10 +0100

    Final version before Application Class övning

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,37 +2,26 @@
 using Microsoft.Extensions.Configuration;
 using SkysDBDemoDatabas;
 using ConsoleApp1.Data;
+using Microsoft.EntityFrameworkCore;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        // 1: Först installerar vi två nuget paket
-        //      Microsoft.EntityFrameworkCore.SqlServer
-        //      Microsoft.EntityFrameworkCore.Tools
-
-        // 2: Sedan kopierar vi i denna text till nuget console
-        // "Server=localhost;Database=RichardsAuctionSite;Trusted_Connection=True;TrustServerCertificate=true" Microsoft.EntityFrameworkCore.SqlServer - OutputDir Data
-
-        // 3: Nu har vi möjlighet att loopa igenom datat i vår databas
-        //using (var dbContext = new RichardsAuctionSiteContext())
-        //{
-        //    foreach (var user in dbContext.Users.OrderBy(u => u.City))
-        //    {
-        //        Console.WriteLine($"{user.Name}: {user.City}");
-        //    }
-        //}
-
-        //// 6: Create a builder to access appsettings.json
         var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
         var config = builder.Build();
 
-        //// Hämta connection string från appsettings.json
         var connectionString = config.GetConnectionString("DefaultConnection");
 
-        //// Starta vår app med connectionstring som en parameter
-        var application = new Application(connectionString);
-        application.Run();
+        var options = new DbContextOptionsBuilder<RichardsAuctionSiteContext>();
+        options.UseSqlServer(connectionString);
 
+        using (var dbContext = new RichardsAuctionSiteContext(options.Options))
+        {
+            foreach (var user in dbContext.Users.OrderBy(u => u.City))
+            {
+                Console.WriteLine($"{user.Name}: {user.City}");
+            }
+        }
     }
 }
